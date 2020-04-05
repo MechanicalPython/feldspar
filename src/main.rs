ef2835dac7feb1264be543cb34aae512507a9659
Matt Barson <matt.barson@gmail.com>
2020-04-05 14:55:26 +0100

    removed speed in knots.

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn feldspar_gps() {
                             gps_values.timestamp, gps_values.latitude,
                              gps_values.longitude, gps_values.fix_quality,
                              gps_values.satellites, gps_values.altitude_m,
-                             gps_values.speed_knots, gps_values.horizontal_dilution)
+                             gps_values.horizontal_dilution)
                         .as_bytes()).expect("Failed to write line");
             }
         }
