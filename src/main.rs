6ff4887c45eb72f7d7800ad13f9a4543f94ba790
Matt Barson <matt.barson@gmail.com>
2020-04-22 13:56:31 +0100

    Increase gps update rate

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn feldspar_gps(capture_duration: u64, file_name: &str) {
     let mut gps = Gps { port };
 
     gps.pmtk_314_api_set_nmea_output(0, 0, 1, 1, 1, 1, 1);
-
+    gps.pmtk_220_set_nmea_updaterate("100");
     let _file = OpenOptions::new()
         .write(true)
         .create_new(true)
