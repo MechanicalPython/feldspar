f8a3b3cd4889caf24253f22ee8ecf559369cf019
Matt Barson <matt.barson@gmail.com>
2020-04-22 17:07:00 +0100

    debug compile

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ fn feldspar_parachute(_seconds_to_wait: u64) {
     pin.set_pwm(
         Duration::from_millis(PERIOD_MS),
         Duration::from_micros(PULSE_MAX_US),
-    )?;
+    ).unwrap();
 
     // Sleep for 500 ms while the servo moves into position.
     thread::sleep(Duration::from_millis(500));
