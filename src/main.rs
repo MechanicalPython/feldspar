1ca5ab5ccc786495e2244d7cead210d6711b704f
Matt Barson <matt.barson@gmail.com>
2020-04-30 14:30:56 +0100

    Easy way to include just navigation data and/or satellite data.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ use rppal::gpio::Gpio;
 fn feldspar_gps(capture_duration: u64, file_name: &str) -> f32 {
     let port = open_port("/dev/serial0", 9600);
     let mut gps = Gps { port };
-    gps.pmtk_251_set_nmea_baudrate()
+
+    gps.pmtk_251_set_nmea_baudrate();
     gps.pmtk_314_api_set_nmea_output(0, 0, 1, 1, 1, 1, 1);
     let _pmtk001 = gps.pmtk_220_set_nmea_updaterate("1000");
     let _file = OpenOptions::new()
