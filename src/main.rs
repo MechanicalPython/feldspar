074cd0b39f63e863cfe8970b040ad3e8e9e0ef1f
Matt Barson <matt.barson@gmail.com>
2020-05-07 11:54:46 +0100

    fix cam recording length

--- src/main.rs
+++ src/main.rs
@@ -131,7 +131,7 @@ fn feldspar_cam(vid_file: &str) -> Output {
         .arg("-o")
         .arg(vid_file)
         .arg("-t")
-        .arg("360000")
+        .arg("3600000")
         .output()
         .expect("Camera failed to open.");
     return c
