2ed6f07cc49bf438243667cf350b620a670aeaa4
Matt Barson <matt.barson@gmail.com>
2020-05-07 19:28:24 +0100

    update gps.rs

--- examples/gps.rs
+++ examples/gps.rs
@@ -1,23 +1,26 @@
-use adafruit_gps::gps::{GetGpsData, Gps, open_port};
-use adafruit_gps::PMTK::send_pmtk::SendPmtk;
+use adafruit_gps::gps::{Gps, GpsSentence, open_port};
 
 fn main() {
     let port = open_port("/dev/serial0", 57600);
     let mut gps = Gps { port };
 
-    gps.pmtk_314_api_set_nmea_output(0, 0, 1, 1, 1, 1, 1);
+    gps.pmtk_314_api_set_nmea_output(0, 0, 0, 1, 0, 0, 1);
     let _pmtk001 = gps.pmtk_220_set_nmea_updaterate("100");
 
     loop {
         let gps_values = gps.update();
-        println!(
-                "{:?},{:?},{:?},{:?},{:?},{:?}\n",
-                gps_values.utc,
-                gps_values.latitude,
-                gps_values.longitude,
-                gps_values.speed_kph,
-                gps_values.geoidal_spe,
-                gps_values.altitude
-            );
+        match gps_values {
+            GpsSentence::GGA(sentence) => {
+                println!(
+                    "{:?},{:?},{:?},{:?},{:?},{:?}\n",
+                    sentence.utc,
+                    sentence.lat,
+                    sentence.long,
+                    sentence.geoidal_sep,
+                    sentence.msl_alt,
+                );
+            }
+            _ => {}
+        }
     }
 }
