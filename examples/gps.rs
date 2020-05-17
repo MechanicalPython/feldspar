6e3242bbe45aab4503ff8ceee5634017bd5d5645
Matt Barson <matt.barson@gmail.com>
2020-05-17 18:17:35 +0100

    change gps example baud rate to 9600

--- examples/gps.rs
+++ examples/gps.rs
@@ -1,11 +1,10 @@
 use adafruit_gps::gps::{Gps, GpsSentence, open_port};
 
 fn main() {
-    let port = open_port("/dev/serial0", 57600);
+    let port = open_port("/dev/serial0", 9600);
     let mut gps = Gps { port };
 
     gps.pmtk_314_api_set_nmea_output(0, 0, 0, 1, 0, 0, 1);
-    let _pmtk001 = gps.pmtk_220_set_nmea_updaterate("100");
 
     loop {
         let gps_values = gps.update();
